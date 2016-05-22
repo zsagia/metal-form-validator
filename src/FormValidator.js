30ee026f2c2e37db667de485b6489ee5b0127af4
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-22 16:39:36 +0200

    Init

---
+++ src/FormValidator.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import State from 'metal-state';
+
+class FormValidator extends State  {
+}
+
+export default FormValidator;
---
+++ test/FormValidator.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import FormValidator from '../src/FormValidator';
+
+describe('FormValidator', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
