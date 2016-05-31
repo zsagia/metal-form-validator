aecca5e05de57eb21ecc97d102c326444358782c
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-31 15:54:50 +0200

    version2

--- src/FormElementValidator.js
+++ src/FormElementValidator.js
@@ -2,117 +2,160 @@
 
 import core from 'metal';
 import dom from 'metal-dom';
-import Constraints from './Constraints';
 import State from 'metal-state';
 
+/**
+ * Element validator instance for to validate a form element.
+ */
 class FormElementValidator extends State  {
 	constructor(opt_config) {
 		super(opt_config);
 
-		this.constraints = [];
-
-		let instance = this;
-		let formElement = this.formElement;
-		let type = this.type;
+		var instance = this;
+		let formElement = instance.formElement;
 		
-		this.setForm_(formElement);
-		this.setNativeValidation_(formElement);
-		this.checkTypeConsistency_(formElement);
-		this.addConstraints_();
+		instance.setForm_(formElement);
+		instance.createErrorContainer_();
 
 		dom.on(formElement, 'input', function(event) {
-			 if (event.target.matches(':invalid')) {
-              console.log('input:invalid');
-            } else {
-               console.log('input:valid');
-            }
-		});
+			if (event.target.matches(':invalid')) {
+				var errorContainer = instance.addErrorContainer_();
+
+				this.addFieldErrors(event.target.validity);
+
+				console.log('input:invalid');
+			}
+			else {
+				var errorContainer = this.errorContainer_;
+
+				if (errorContainer) {
+					errorContainer.remove();
+					errorContainer = null;
+				}
+
+				console.log('input:valid');
+			}
+		}.bind(this));
 
 		dom.on(formElement, 'blur', function(event) {
-            if (event.target.matches(':invalid')) {
-              console.log('blur:invalid');
-            } else {
-               console.log('blur:valid');
-            }
-		});
+			if (event.target.matches(':invalid')) {
+				var errorContainer = instance.addErrorContainer_();
+
+				this.addFieldErrors(event.target.validity);
+
+				console.log('blur:invalid');
+			}
+			else {
+				var errorContainer = this.errorContainer_;
+
+				if (errorContainer) {
+					errorContainer.remove();
+					errorContainer = null;
+				}
+
+				console.log('blur:valid');
+			}
+		}.bind(this));
+
+		dom.on(formElement, 'valid', function(event) {
+			if (event.target.matches(':valid')) {
+				console.log('valid');
+			}
+		}.bind(this));
 
 		dom.on(formElement, 'invalid', function(event) {
-			let formElement = event.currentTarget;
-			event.preventDefault();
-			console.log('invalid');
-		});
+			var errorContainer = instance.addErrorContainer_();
+
+			if (event.target.matches(':invalid')) {
+				event.preventDefault();
+
+				this.addFieldErrors(event.target.validity);
+
+				console.log('invalid');
+			}
+		}.bind(this));
 	}
 
 	/**
-	 * Adds constraints based on the element's type.
+	 *
 	 */
-	addConstraints_() {
-		let type = this.type,
-			constraint;
+	createErrorContainer_() {
+		var errorContainer;
 
-		if (this.isNativeValidation_) {
-			switch (type) {
-				case 'email':
-					constraint = Constraints.email;
-					break;
-				case 'url':
-					constraint = Constraints.url;
-					break;
-				case 'required':
-					constraint = Constraints.required;
-					break;
-			}
+		if(!this.errorContainer_) {
+			errorContainer = document.createElement('div');
 
-			if (this.isRequired_()) {
-				this.constraints.push(Constraints.required);
-			}
-
-			if (constraint) {
-				this.constraints.push(constraint);
-			}
+			errorContainer.className = "form-validator-stack help-block";
 		}
 
-		var custom = this.custom;
-
-		if (custom && core.isFunction(custom)) {
-			this.constraints.push(custom);
-		}
+		return errorContainer;
 	}
 
-	isRequired_() {
-		return this.formElement.required;
-	}
+	addErrorContainer_() {
+		var errorContainer = this.errorContainer_;
 
-	/**
-	 * Re-check needed
-	 */
-	checkTypeConsistency_(formElement) {
-		let type = this.type,
-			formElementType = formElement.getAttribute('type');
+		if (!errorContainer) {
+			errorContainer = this.createErrorContainer_();
 
-		if(type && formElementType && type !== formElementType) {
-			throw new Error('Inconsistent types!');
+			let parentElement = dom.closest(this.formElement, '.form-group');
+
+			if (parentElement) {
+				parentElement.appendChild(errorContainer);
+			}
+			else {
+				let nextSibling = this.formElement.nextElementSibling;
+
+				if (nextSibling) {
+					this.form_.insertBefore(errorContainer, nextSibling);
+				}
+				else {
+					this.form_.appendChild(errorContainer);
+				}
+			}
+
+			this.errorContainer_ = errorContainer;
 		}
-
-		type = type || formElementType || (this.custom ? 'custom' : undefined);
-
-		if (!type) {
-			throw new Error('Invalid type');
-		}
-		else {
-			this.type = type;
-		}
+		
+		return errorContainer;
 	}
 
 	/**
-	 * Calls original checkValidity() on the form element and the custom validation
+	 * Adds a validation error in the field.
+	 *
+	 * @method addFieldError
+	 * @param {Node} field
+	 * @param ruleName
 	 */
-	validate_(validity) {
-		if (validity.valid) {
-			this.constraints.forEach(
-				function(item){
-					item();
-				});
+	addFieldError(ruleName) {
+		var errorContainer = this.errorContainer_;
+		var divElement = document.createElement('div');
+
+		divElement.innerHTML = this.strings[ruleName];
+		errorContainer.appendChild(divElement);
+	}
+
+	/**
+	 * Adds a validation error in the field.
+	 *
+	 * @method addFieldError
+	 * @param {Node} field
+	 * @param ruleName
+	 */
+	addFieldErrors(validity) {
+		var requiedFailed = validity['valueMissing'];
+
+		if (requiedFailed) {
+			this.addFieldError('required');
+		}
+
+		if (this.showAllMessages) {
+			for(errorKey in validity) {
+				let errorValue = validity[errorKey];
+
+				if (errorValue) {
+					this.addFieldError(errorKey);
+				}
+			}
 		}
 	}
 
@@ -124,51 +167,92 @@ class FormElementValidator extends State  {
 	}
 
 	/**
-	 *
+	 * 
 	 */
 	setFormElement_(value) {
-		var formElement = document.getElementById(value);
-
-		return formElement;
-	}
-
-	/**
-	 * Checks native validation is weather or not.
-	 */
-	setNativeValidation_(formElement) {
-		this.isNativeValidation_ = typeof formElement.willValidate !== "undefined";
+		return core.isElement(value) ? value : document.getElementById(value);
 	}
 }
 
 FormElementValidator.STATE = {
 	/**
-	 * Extends base constraints with a custom logic.
-	 */
-	custom: {
-
-	},
-
-	/**
-	 *
+	 * An html form element which will be validated.
+	 * Setter method always 
+	 * @type {string|node}
 	 */
 	formElement: {
 		setter: 'setFormElement_'
 	},
 
 	/**
+	 * Defines if all validation messages will be showed or not.
 	 *
+	 * @attribute showAllMessages
+	 * @default false
+	 * @type Boolean
 	 */
-	forceNativeValidation: {
+	showAllMessages: {
+		validator: core.isBoolean,
+		value: false
+	},
+
+	/**
+	 * Defines if the validation messages will be showed or not.
+	 *
+	 * @attribute showMessages
+	 * @default true
+	 * @type Boolean
+	 */
+	showMessages: {
 		validator: core.isBoolean,
 		value: true
 	},
 
 	/**
+	 * Collection of strings used to label elements of the UI.
 	 *
+	 * @attribute strings
+	 * @type Object
 	 */
-	type: {
+	strings: {
+		validator: core.isObject,
+		value: {
+			email: 'Please enter a valid email address in {field}.',
+			max: 'Please enter a value less than or equal to {0} in {field}.',
+			maxLength: 'Please enter no more than {0} characters in {field}.',
+			min: 'Please enter a value greater than or equal to {0} in {field}.',
+			minLength: 'Please enter at least {0} characters in {field}.',
+			number: 'Please enter a valid number in {field}.',
+			range: 'Please enter a value between {0} and {1} in {field}.',
+			rangeLength: 'Please enter a value between {0} and {1} characters long in {field}.',
+			required: '{field} is required.',
+			url: 'Please enter a valid URL in {field}.'
+		}
+	},
 
-	}
+	/**
+	 * If `true` the field will be validated on blur event.
+	 *
+	 * @attribute validateOnBlur
+	 * @default true
+	 * @type Boolean
+	 */
+	validateOnBlur: {
+		validator: core.isBoolean,
+		value: true
+	},
+
+	/**
+	 * If `true` the field will be validated on input event.
+	 *
+	 * @attribute validateOnInput
+	 * @default false
+	 * @type Boolean
+	 */
+	validateOnInput: {
+		validator: core.isBoolean,
+		value: false
+	},
 };
 
 export default FormElementValidator;
--- src/all/formvalidator.js
+++ src/all/formvalidator.js
@@ -1,7 +1,6 @@
 'use strict';
 
-import Constraints from '../Constraints';
 import FormElementValidator from '../FormElementValidator';
 import FormValidator from '../FormValidator';
 
-export { Constraints, FormElementValidator, FormValidator };
+export { FormElementValidator, FormValidator };
